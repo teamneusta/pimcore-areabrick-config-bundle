732f4a7b157ad17deae8228e4069d9e73cf5acdd
Michael Albrecht <m.albrecht@neusta.de>
2023-01-25 10:52:05 +0100

    phpstan fixed

--- tests/Unit/EditableDialogBox/EditableItem/NumericItemTest.php
+++ tests/Unit/EditableDialogBox/EditableItem/NumericItemTest.php
@@ -1,19 +1,21 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Neusta\Pimcore\AreabrickConfigBundle\Tests\Unit\EditableDialogBox\EditableItem;
 
 use Neusta\Pimcore\AreabrickConfigBundle\EditableDialogBox\DialogBoxItem;
 use Neusta\Pimcore\AreabrickConfigBundle\EditableDialogBox\EditableItem\NumericItem;
-use PHPUnit\Framework\TestCase;
+
 use function PHPUnit\Framework\assertEquals;
+
+use PHPUnit\Framework\TestCase;
 
 class NumericItemTest extends TestCase
 {
-    const ITEM_TEST_LABEL = 'test';
-    const MIN_VALUE = 12;
-    const MAX_VALUE = 24;
-    const REGULAR_DEFAULT_VALUE = 20;
-    const OUTOFBOUNDS_DEFAULT_VALUE = 30;
+    public const ITEM_TEST_LABEL = 'test';
+    public const MIN_VALUE = 12;
+    public const MAX_VALUE = 24;
+    public const REGULAR_DEFAULT_VALUE = 20;
+    public const OUTOFBOUNDS_DEFAULT_VALUE = 30;
 
     /**
      * @test
@@ -27,11 +29,10 @@ class NumericItemTest extends TestCase
                 'type' => 'numeric',
                 'name' => self::ITEM_TEST_LABEL,
                 'config' => [
-
                     'defaultValue' => self::MIN_VALUE,
                     'minValue' => self::MIN_VALUE,
                     'maxValue' => self::MAX_VALUE,
-                ]
+                ],
             ],
             $item->toArray()
         );
@@ -40,7 +41,7 @@ class NumericItemTest extends TestCase
     /**
      * @test
      */
-    public function setDefaultValue_regular_case(): void
+    public function setDefaultValueRegularCase(): void
     {
         $item = new NumericItem(self::ITEM_TEST_LABEL, self::MIN_VALUE, self::MAX_VALUE);
         $item->setDefaultValue(self::REGULAR_DEFAULT_VALUE);
@@ -49,11 +50,10 @@ class NumericItemTest extends TestCase
                 DialogBoxItem::ITEM_TYPE => 'numeric',
                 DialogBoxItem::ITEM_NAME => self::ITEM_TEST_LABEL,
                 DialogBoxItem::ITEM_CONFIG => [
-
                     'defaultValue' => self::REGULAR_DEFAULT_VALUE,
                     'minValue' => self::MIN_VALUE,
                     'maxValue' => self::MAX_VALUE,
-                ]
+                ],
             ],
             $item->toArray()
         );
@@ -62,7 +62,7 @@ class NumericItemTest extends TestCase
     /**
      * @test
      */
-    public function setDefaultValue_outofbounds_case(): void
+    public function setDefaultValueOutofboundsCase(): void
     {
         $item = new NumericItem(self::ITEM_TEST_LABEL, self::MIN_VALUE, self::MAX_VALUE);
         $item->setDefaultValue(self::OUTOFBOUNDS_DEFAULT_VALUE);
@@ -71,11 +71,10 @@ class NumericItemTest extends TestCase
                 'type' => 'numeric',
                 'name' => self::ITEM_TEST_LABEL,
                 'config' => [
-
                     'defaultValue' => self::MIN_VALUE,
                     'minValue' => self::MIN_VALUE,
                     'maxValue' => self::MAX_VALUE,
-                ]
+                ],
             ],
             $item->toArray()
         );
