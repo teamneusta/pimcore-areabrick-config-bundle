dd58d53c3ead1776fb449dfadb1e258f82613363
Stephanie Ketzler <s.ketzler@neusta.de>
2024-10-02 11:15:52 +0200

    [Areabrick Overview] adding areabrick-overview-unpublished-toggle.js

--- src/EventListener/PimcoreAdminListener.php
+++ src/EventListener/PimcoreAdminListener.php
@@ -18,6 +18,7 @@ final class PimcoreAdminListener
         $event->addPaths([
             '/bundles/neustapimcoreareabrickconfig/js/startup.js',
             '/bundles/neustapimcoreareabrickconfig/js/areabrick-overview.js',
+            '/bundles/neustapimcoreareabrickconfig/js/areabrick-overview-unpublished-toggle.js',
         ]);
     }
 }
