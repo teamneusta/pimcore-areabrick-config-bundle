db248eecd4cb1ad18372afa16bc343684319754b
Jacob Dreesen <j.dreesen@neusta.de>
2024-10-02 18:43:35 +0200

    feat: automatic colors

--- public/js/areabrick-overview.js
+++ public/js/areabrick-overview.js
@@ -38,6 +38,22 @@ neusta.areabrick_config.areabrick_overview = Class.create({
                         html: response.responseText,
                         autoScroll: true,
                     });
+
+                    document.getElementById(this.tabId)
+                        .querySelectorAll('#neusta_areabrick_config .accordion a')
+                        .forEach(el => {
+                            const bgColor = this.toHSL(el.textContent);
+                            // The threshold at which colors are considered "light."
+                            // Range: integers from 0 to 100, recommended 50-70.
+                            // Any lightness value below the threshold will result in white,
+                            // any above will result in black.
+                            const threshold = 50;
+
+                            el.style.backgroundColor = `hsl(${bgColor.h}, ${bgColor.s}%, ${bgColor.l}%)`;
+                            // https://css-tricks.com/switch-font-color-for-different-backgrounds-with-css/#aa-applying-the-trick-to-our-font-color-declaration
+                            // soon™: https://developer.mozilla.org/en-US/docs/Web/CSS/color_value/color-contrast
+                            el.style.color = `hsl(0, 0%, calc((${bgColor.l} - ${threshold}) * -100%))`;
+                        })
                 }.bind(this),
             });
 
@@ -57,6 +73,33 @@ neusta.areabrick_config.areabrick_overview = Class.create({
                 handler(el, event);
             }
         });
+    },
+
+    // https://gist.github.com/0x263b/2bdd90886c2036a1ad5bcf06d6e6fb37
+    toHSL: function (string, opts) {
+        opts = opts || {};
+        opts.hue = opts.hue || [0, 360];
+        opts.sat = opts.sat || [75, 100];
+        opts.lit = opts.lit || [40, 60];
+
+        const range = function (hash, min, max) {
+            const diff = max - min
+            const x = ((hash % diff) + diff) % diff
+
+            return x + min
+        };
+
+        let hash = 0
+        for (let i = 0; i < string.length; i++) {
+            hash = string.charCodeAt(i) + ((hash << 5) - hash);
+            hash = hash & hash;
+        }
+
+        const h = range(hash, opts.hue[0], opts.hue[1]);
+        const s = range(hash, opts.sat[0], opts.sat[1]);
+        const l = range(hash, opts.lit[0], opts.lit[1]);
+
+        return {h, s, l};
     },
 
 });
