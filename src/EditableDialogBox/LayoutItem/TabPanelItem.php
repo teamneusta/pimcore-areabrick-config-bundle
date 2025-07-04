ff7a40e5a1d503c16fd09d62f47d33ec083df9c0
Jacob Dreesen <j.dreesen@neusta.de>
2025-07-04 17:38:37 +0200

    feat: add support for translatable tab titles in `TabPanelItem` class

--- src/EditableDialogBox/LayoutItem/TabPanelItem.php
+++ src/EditableDialogBox/LayoutItem/TabPanelItem.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 namespace Neusta\Pimcore\AreabrickConfigBundle\EditableDialogBox\LayoutItem;
 
 use Neusta\Pimcore\AreabrickConfigBundle\EditableDialogBox\LayoutItem;
+use Symfony\Component\Translation\IdentityTranslator;
+use Symfony\Contracts\Translation\TranslatableInterface;
 
 /**
  * @extends LayoutItem<PanelItem>
  */
 class TabPanelItem extends LayoutItem
 {
+    private readonly IdentityTranslator $translator;
+
     /** @var array<string, PanelItem> */
     private array $items;
 
@@ -19,15 +23,31 @@ class TabPanelItem extends LayoutItem
     public function __construct(array $items = [])
     {
         parent::__construct('tabpanel', $items);
-        $this->items = array_column($items, null, 'title');
+
+        $this->translator = new IdentityTranslator();
+
+        foreach ($items as $item) {
+            $this->items[$this->toString($item->title)] = $item;
+        }
     }
 
-    public function getOrCreateTab(string $title): PanelItem
+    public function getOrCreateTab(string|TranslatableInterface $title): PanelItem
     {
-        if (!isset($this->items[$title])) {
-            $this->addItem($this->items[$title] = new PanelItem($title));
+        $key = $this->toString($title);
+
+        if (!isset($this->items[$key])) {
+            $this->addItem($this->items[$key] = new PanelItem($title));
         }
 
-        return $this->items[$title];
+        return $this->items[$key];
+    }
+
+    private function toString(string|TranslatableInterface $text): string
+    {
+        if ($text instanceof TranslatableInterface) {
+            return $text->trans($this->translator);
+        }
+
+        return $text;
     }
 }
