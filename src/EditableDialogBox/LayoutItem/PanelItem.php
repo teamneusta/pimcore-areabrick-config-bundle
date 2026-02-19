10a20df437589514fe3f31640b985cd24635a77a
Jacob Dreesen <j.dreesen@neusta.de>
2026-02-19 16:41:57 +0100

    feat: add `addEditable` method to `PanelItem` and deprecate `addItem` as well as passing non-editable items to the constructor

--- src/EditableDialogBox/LayoutItem/PanelItem.php
+++ src/EditableDialogBox/LayoutItem/PanelItem.php
@@ -17,26 +17,57 @@ class PanelItem extends LayoutItem implements \IteratorAggregate
     public readonly string $title;
 
     /** @var array<string, EditableItem> */
-    private array $editableItems;
+    private array $items;
 
     /**
      * @param list<DialogBoxItem> $items
      */
     public function __construct(string $title, array $items = [])
     {
-        parent::__construct('panel', []);
+        parent::__construct('panel', $items);
         $this->title = $title;
-        $this->addItem(...$items);
+
+        $deprecationTriggered = false;
+        foreach ($items as $item) {
+            if ($item instanceof EditableItem) {
+                $this->items[$item->name()] = $item;
+            } elseif (!$deprecationTriggered) {
+                trigger_deprecation('teamneusta/pimcore-areabrick-config-bundle', '2.2.0', 'Not passing only "%s" to %s() is deprecated.', EditableItem::class, __METHOD__);
+                $deprecationTriggered = true;
+            }
+        }
     }
 
+    /**
+     * @deprecated since version 2.2.0, use `addEditable()` instead.
+     */
     public function addItem(DialogBoxItem ...$items): static
     {
+        trigger_deprecation('teamneusta/pimcore-areabrick-config-bundle', '2.2.0', 'The method "%s()" is deprecated, use "%s::addEditable()" instead.', __METHOD__, static::class);
+
         foreach ($items as $item) {
             parent::addItem($item);
 
             if ($item instanceof EditableItem) {
-                $this->editableItems[$item->name()] = $item;
+                $this->items[$item->name()] = $item;
             }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function addEditable(EditableItem ...$items): static
+    {
+        foreach ($items as $item) {
+            if (isset($this->items[$item->name()])) {
+                throw new \InvalidArgumentException(\sprintf('Editable item with name "%s" already exists.', $item->name()));
+            }
+
+            $this->items[$item->name()] = $item;
+            parent::addItem($item);
         }
 
         return $this;
@@ -44,22 +75,25 @@ class PanelItem extends LayoutItem implements \IteratorAggregate
 
     public function hasEditable(string $name): bool
     {
-        return isset($this->editableItems[$name]);
+        return isset($this->items[$name]);
     }
 
     public function getEditable(string $name): EditableItem
     {
-        return $this->editableItems[$name]
+        return $this->items[$name]
             ?? throw new \InvalidArgumentException(\sprintf('Editable item with name "%s" not found.', $name));
     }
 
+    /**
+     * @return $this
+     */
     public function removeEditable(string $name): static
     {
-        if (!$item = $this->editableItems[$name] ?? null) {
+        if (!$item = $this->items[$name] ?? null) {
             throw new \InvalidArgumentException(\sprintf('Editable item with name "%s" not found.', $name));
         }
 
-        unset($this->editableItems[$name]);
+        unset($this->items[$name]);
 
         return $this->removeItem($item);
     }
@@ -69,7 +103,7 @@ class PanelItem extends LayoutItem implements \IteratorAggregate
      */
     public function getIterator(): \ArrayIterator
     {
-        return new \ArrayIterator(array_values($this->editableItems));
+        return new \ArrayIterator(array_values($this->items));
     }
 
     protected function getAttributes(): array
