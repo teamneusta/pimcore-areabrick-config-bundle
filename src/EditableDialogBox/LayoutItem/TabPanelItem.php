b7ab5577d12a3965a349fe2820fd76459329b037
Jacob Dreesen <j.dreesen@neusta.de>
2026-02-19 16:39:42 +0100

    feat: add `addTab` method to `TabPanelItem` and deprecate `getOrCreateTab`

--- src/EditableDialogBox/LayoutItem/TabPanelItem.php
+++ src/EditableDialogBox/LayoutItem/TabPanelItem.php
@@ -24,13 +24,32 @@ class TabPanelItem extends LayoutItem implements \IteratorAggregate
         $this->items = array_column($items, null, 'title');
     }
 
+    /**
+     * @deprecated since version 2.2.0, use `hasTab()` and `addTab()` instead.
+     */
     public function getOrCreateTab(string $title): PanelItem
     {
+        trigger_deprecation('teamneusta/pimcore-areabrick-config-bundle', '2.2.0', 'The method "%s()" is deprecated, use "%2$s::hasTab()" and "%2$s::addTab()" instead.', __METHOD__, static::class);
+
         if (!isset($this->items[$title])) {
             $this->addItem($this->items[$title] = new PanelItem($title));
         }
 
         return $this->items[$title];
+    }
+
+    /**
+     * @return $this
+     */
+    public function addTab(PanelItem $item): static
+    {
+        if ($this->hasTab($item->title)) {
+            throw new \InvalidArgumentException(\sprintf('Tab with title "%s" already exists.', $item->title));
+        }
+
+        $this->items[$item->title] = $item;
+
+        return $this->addItem($item);
     }
 
     public function hasTab(string $title): bool
