b2f740a6d6bc436cc631955f1d7b0b2c04c702d3
Jacob Dreesen <j.dreesen@neusta.de>
2026-02-20 18:31:51 +0100

    feat: improve dialog box configurator validation in `HasDialogBox`
    - Ensure the referenced configurator service exists and throw an exception if not.
    - Enforce that configurators implement the `DialogBoxConfigurator` interface, raising an exception when the type is invalid.

--- src/HasDialogBox.php
+++ src/HasDialogBox.php
@@ -20,10 +20,28 @@ trait HasDialogBox
 
         $this->buildDialogBox($builder, $area, $info);
 
-        if ($configurator = $info?->getParam('dialogBoxConfigurator')) {
-            if ($this->container->has($configurator)) {
-                $this->container->get($configurator)->configureDialogBox($builder, $area, $info);
+        if ($configuratorServiceName = $info?->getParam('dialogBoxConfigurator')) {
+            if (!$this->container->has($configuratorServiceName)) {
+                throw new \LogicException(\sprintf(
+                    'The service "%s" referenced via "params.%s.dialogBoxConfigurator" does not exist.',
+                    $configuratorServiceName,
+                    $info->getId(),
+                ));
             }
+
+            $configurator = $this->container->get($configuratorServiceName);
+
+            if (!$configurator instanceof DialogBoxConfigurator) {
+                throw new \LogicException(\sprintf(
+                    'The service "%s" referenced via "params.%s.dialogBoxConfigurator" must implement "%s", "%s" given.',
+                    $configuratorServiceName,
+                    $info->getId(),
+                    DialogBoxConfigurator::class,
+                    $configurator::class,
+                ));
+            }
+
+            $configurator->configureDialogBox($builder, $area, $info);
         }
 
         return $builder->build();
